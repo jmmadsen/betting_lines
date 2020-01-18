513097a74a3a5ceca48270ef7b2c12fe0b3c2bba
Jacob Madsen <jmadsen24@Jacobs-MacBook-Pro.local>
2020-01-18 17:35:18 -0500

    set up some basic express routing

--- server.js
+++ server.js
@@ -5,10 +5,38 @@ const port = 3000;
 const key = require('./apiKey');
 const apiKey = key.apiKey;
 
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/test', async(req, res, next) => {
-  res.send('dumbass');
-});
+app.get('/sports', async (req, res) => {
+  try {
+    const { data } = await axios.get('https://api.the-odds-api.com/v3/sports', {
+      params: {
+        api_key: apiKey
+      }
+    });
+    res.send(data.data);
+  } catch(err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/nfl_moneyline', async (req, res) => {
+  try {
+    const { data } = await axios.get('https://api.the-odds-api.com/v3/odds', {
+      params: {
+          api_key: apiKey,
+          sport: 'americanfootball_nfl',
+          region: 'us',
+          mkt: 'h2h'
+      }
+    })
+    res.send(data.data)
+  } catch(err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+})
+
+app.listen(port, () => console.log(`Betting lines listening on port ${port}!`));

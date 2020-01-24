d80bce53bc5812198886058e0ce2200e03d45d04
Jacob Madsen <jmadsen24@Jacobs-MBP.lan1>
2020-01-24 00:36:46 -0500

    moved express routes to separate file

---
+++ router.js
@@ -0,0 +1,106 @@
+const { knex } = require('./knexfile');
+const express = require('express');
+const router = express.Router();
+const { scrapeLines } = require('./scrapeLines');
+const { sendEmail } = require('./sendEmail');
+
+
+// send all from sports_seasons table
+router.get('/sports_in_season', async (req, res) => res.send(await knex('sports_seasons').select()));
+
+// adds sport to sports_seasons table
+router.post('/add_sport_in_season', async (req, res) => { 
+  try {
+    const startDate = new Date(req.query.startDate);
+    const endDate = new Date(req.query.endDate);
+    await knex('sports_seasons').insert({ sport: req.query.sport, startDate, endDate });
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// update the season dates for a sport
+router.put('/update_sport_season_dates', async (req, res) => {
+  try {
+    const startDate = new Date(req.query.startDate);
+    const endDate = new Date(req.query.endDate);
+    await knex('sports_seasons').where('sport', req.query.sport).update({ startDate, endDate });
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// remove a sport from sports_seasons table
+router.delete('/remove_sport_in_season', async (req, res) => {
+  try {
+    await knex('sports_seasons').delete().where('sport', req.query.sport);
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// sends current mailList array
+router.get('/mail_list', async (req, res) => {
+  try {
+    const result = await knex('emails_list').select();
+    res.send(result);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// adds email to emailList array
+router.post('/add_email', async (req, res) => {
+  try {
+    await knex('emails_list').insert({email: req.query.email});
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// remove an email from mailList array
+router.delete('/remove_email', async (req, res) => {
+  try {
+    await knex('emails_list').delete().where('email', req.query.email);
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+// route to manually trigger testing while hosted in heroku
+router.post('/test_email', async (req, res) => {
+  try {
+    // fetch emails from DB
+    const mailList = await knex('emails_list').select().map(row => row.email);
+
+    // fetch sports in season from DB
+    let sportsInSeason = await knex('sports_seasons')
+      .select('sport')
+      .where('startDate', '<=', new Date())
+      .andWhere('endDate', '>=', new Date())
+      .map(row => row.sport);
+
+    // order of precedence which they routerear in email
+    const sportsOrder = ['nfl', 'ncaaf', 'nba', 'ncaam', 'mlb', 'nhl'];
+    sportsInSeason.sort((a, b) => {
+      return sportsOrder.indexOf(a) - sportsOrder.indexOf(b);
+    });
+
+    // scrapes daily odds from website
+    let betsObject = await scrapeLines(sportsInSeason);
+    
+    // sends email
+    sendEmail(betsObject, mailList);
+
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
+})
+
+exports.router = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cron = require('node-cron');
 const app = express();
 const { knex } = require('./knexfile');
+const { router } = require('./router');
 const { scrapeLines } = require('./scrapeLines');
 const { sendEmail } = require('./sendEmail');
 
@@ -16,16 +17,37 @@ const { sendEmail } = require('./sendEmail');
 // cron job to control when email is sent
 cron.schedule('00 00 10 * * *', async () => {
 
-  // scrapes daily odds from website
-  let betsObject = await scrapeLines(sportsInSeason);
-  
-  // sends email
-  sendEmail(betsObject, mailList);
+  try {
+    // fetch emails from DB
+    const mailList = await knex('emails_list').select().map(row => row.email);
+
+    // fetch sports in season from DB
+    let sportsInSeason = await knex('sports_seasons')
+      .select('sport')
+      .where('startDate', '<=', new Date())
+      .andWhere('endDate', '>=', new Date())
+      .map(row => row.sport);
+
+    // order of precedence which they routerear in email
+    const sportsOrder = ['nfl', 'ncaaf', 'nba', 'ncaam', 'mlb', 'nhl'];
+    sportsInSeason.sort((a, b) => {
+      return sportsOrder.indexOf(a) - sportsOrder.indexOf(b);
+    });
+
+    // scrapes daily odds from website
+    let betsObject = await scrapeLines(sportsInSeason);
+    
+    // sends email
+    sendEmail(betsObject, mailList);
+    
+    res.sendStatus(200);
+  } catch(err) {
+    res.send(err);
+  }
 
 },{
   timezone: "America/New_York"
 });
-
 
 // middleware to apply password protection to routes
 app.use((req, res, next) => {
@@ -39,106 +61,10 @@ app.use((req, res, next) => {
 // below is routing for express.js
 app.get('/', (req, res) => res.send('Betting lines is live!'));
 
-// send all from sports_seasons table
-app.get('/sports_in_season', async (req, res) => res.send(await knex('sports_seasons').select()));
+// redirect express routes to another file
+app.use('/router', router);
 
-// adds sport to sports_seasons table
-app.post('/add_sport_in_season', async (req, res) => {
-  try {
-    const startDate = new Date(req.query.startDate);
-    const endDate = new Date(req.query.endDate);
-    await knex('sports_seasons').insert({ sport: req.query.sport, startDate, endDate });
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// update the season dates for a sport
-app.put('/update_sport_season_dates', async (req, res) => {
-  try {
-    const startDate = new Date(req.query.startDate);
-    const endDate = new Date(req.query.endDate);
-    await knex('sports_seasons').where('sport', req.query.sport).update({ startDate, endDate });
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// remove a sport from sports_seasons table
-app.delete('/remove_sport_in_season', async (req, res) => {
-  try {
-    await knex('sports_seasons').delete().where('sport', req.query.sport);
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// sends current mailList array
-app.get('/mail_list', async (req, res) => {
-  try {
-    const result = await knex('emails_list').select();
-    res.send(result);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// adds email to emailList array
-app.post('/add_email', async (req, res) => {
-  try {
-    await knex('emails_list').insert({email: req.query.email});
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// remove an email from mailList array
-app.delete('/remove_email', async (req, res) => {
-  try {
-    await knex('emails_list').delete().where('email', req.query.email);
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-// route to manually trigger testing while hosted in heroku
-app.post('/test_email', async (req, res) => {
-  try {
-    // fetch emails from DB
-    const mailList = await knex('emails_list').select().map(row => row.email);
-
-    // fetch sports in season from DB
-    let sportsInSeason = await knex('sports_seasons')
-      .select('sport')
-      .where('startDate', '<=', new Date())
-      .andWhere('endDate', '>=', new Date())
-      .map(row => row.sport);
-
-    // order of precedence which they appear in email
-    const sportsOrder = ['nfl', 'ncaaf', 'nba', 'ncaam', 'mlb', 'nhl'];
-    sportsInSeason.sort((a, b) => {
-      return sportsOrder.indexOf(a) - sportsOrder.indexOf(b);
-    });
-
-    // scrapes daily odds from website
-    let betsObject = await scrapeLines(sportsInSeason);
-    
-    // sends email
-    sendEmail(betsObject, mailList);
-
-    res.sendStatus(200);
-  } catch(err) {
-    res.send(err);
-  }
-})
-
-
-// below terminate processes and connections
+// below terminates processes and connections
 process.on('uncaughtException', err => {
   console.error(err);
   process.exit();

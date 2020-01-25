177f8f7eaa7941b654c76fdb6165b3669747c0b7
Jacob Madsen <jmadsen24@jacobs-mbp.home>
2020-01-25 10:02:42 -0500

    create docker-compose.yml and modified knexfile.js to set up local db for testing

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,27 @@
-const knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB
-  }
-})
+let knex;
+
+console.log('****************')
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === `production`) {
+  knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB
+    }
+  })
+} else {
+  knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'user',
+      password: 'password',
+      database: 'db'
+    }
+  })
+}
 
 exports.knex = knex;

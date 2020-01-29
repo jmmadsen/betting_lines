46c1cf3f8d53158aaf5982f252eb196607ec0780
Jacob Madsen <jmadsen24@Jacobs-MBP.lan1>
2020-01-28 19:41:27 -0500

    created espn nba scraper

--- processes/createExcel.js
+++ processes/createExcel.js
@@ -8,6 +8,10 @@ const createExcel = async (scrapedSites) => {
     return;
   }
 
+  // ncaam names for tab color
+  const ncaamTabs = ['KenPom Ratings', 'Sonny Power Ratings']
+  const nbaTabs = ['ESPN NBA Stats'];
+
   // initializes workbook
   let workbook = new ExcelJS.Workbook();
   workbook.creator = 'Jacob Madsen';
@@ -15,7 +19,15 @@ const createExcel = async (scrapedSites) => {
 
   // creates a new excel tab for each website
   for (const site of scrapedSites) {
-    let sheet = workbook.addWorksheet(site.name);
+    // choose colors based on tab arrays above
+    let color;
+    if (ncaamTabs.includes(site.name)) {
+      color = '01d50c';
+    } else if (nbaTabs.includes(site.name)) {
+      color = '0180d5';
+    }
+    // adds new sheet to xlsx for each site
+    let sheet = workbook.addWorksheet(site.name, { properties: { tabColor: { argb: color } } });
     sheet.columns = site.columns;
     for (let row of site.data) {
       sheet.addRow(row);
